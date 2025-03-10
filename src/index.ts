88d081bf1ee9e71f1f516760d9a31fde714447a0
Burrito <37681398+burritosoftware@users.noreply.github.com>
2025-03-10 00:50:49 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ export default {
     <html lang="en">
     <head>
       <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0">
       <title>Waymo Promo</title>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
       <style>
